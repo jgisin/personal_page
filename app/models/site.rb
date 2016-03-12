50481e256102c024ee75880bbdd270c01eb9dd99
Jeffrey Gisin <jgisin@gmail.com>
2016-03-11 23:11:00 -0700

    fix

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,5 +1,5 @@
 class Site < ActiveRecord::Base
-  has_attached_file :url_image, :styles => { :preview => "650x500", :thumb => "500x300"}, s3_host_name => 's3-us-west-2.amazonaws.com'
+  has_attached_file :url_image, :styles => { :preview => "650x500", :thumb => "500x300"}, :s3_host_name => 's3-us-west-2.amazonaws.com'
 
   validates_attachment_content_type :url_image, :content_type => /\Aimage\/.*\Z/
 end
